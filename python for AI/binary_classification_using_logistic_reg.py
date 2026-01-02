d36390ffe216d3954f912aa775a157001a36f5ef
md mohsin <siam.mohsin2005@gmail.com>
2026-01-02 12:11:15 +0600

    implement sigmoid

---
+++ python for AI/binary_classification_using_logistic_reg.py
@@ -0,0 +1,13 @@
+import math
+
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+lr = float(input())
+epochs = int(input())
+w = [0.0], b = 0.0, m = len(y)
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+for epoch in epochs:
+    pass
